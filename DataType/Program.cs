5ea277c496b51dbd28582c48f1e675d7cd7379bd
HongEngVathana <engvathanahong@gmail.com>
2024-10-06 20:05:33 +0700

    Hello

---
+++ DataType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+ namespace MyApplication
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 2;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool MyBool = true;
+            string mytext = "Hello my name Hong Engvathana  it's the first to learn C#";
+            Console.WriteLine(myNum);
+            Console.WriteLine(mytext);
+
+
+
+        }
+    }
+}
